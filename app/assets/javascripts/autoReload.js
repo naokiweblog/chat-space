2882889664bbc4bbcd715da762eaf41d48611fa5
naokiweblog <nk1110.chiba@gmail.com>
2020-06-18 11:58:50 +0900

    remove-console.log

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -50,7 +50,6 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log(messages);
       if (messages.length !== 0) {
         let insertHTML = '';
         $.each(messages, function(i, message) {
